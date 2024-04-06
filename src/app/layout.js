3d577506a6d8e1de2b75e81af530ee4c697be395
clientanriyo <client@anriyo.com>
2024-04-06 18:30:01 +0530

    6.4.24

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter, Playfair } from "next/font/google";
 import "./globals.css";
 import { Navbar, Footer } from "@/components/other";
 
 const inter = Inter({ subsets: ["latin"] });
+const playfair = Playfair({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Kandhara Jewellers",
@@ -14,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">
+      <body className={``}>
         <Navbar />
         {children}
         <Footer />
--- src/components/other.js
+++ src/components/other.js
@@ -1,7 +1,11 @@
 'use client'
 import Logo from "../../public/logo.png"
+import Logo2 from "../../public/logo1.png"
 import Image from "next/image"
 import Icon from "../../public/icon.png"
+
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 export const Navbar = () => {
     return (
@@ -42,8 +46,59 @@ export const Navbar = () => {
 
 export const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="p-10 bg-gray-900">
+
+            {/* Main */}
+            <div className="grid grid-cols-2 mt-5">
+                <div>
+                    <Image
+                        src={Logo2}
+                        className="h-16 w-auto drop-shadow-2xl"
+                    />
+                    <div className="text-white my-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+
+                    <div className="flex pt-2">
+                        <a href="#">
+                            <FaInstagram className="text-white text-2xl hover:text-yellow-400 duration-300" />
+                        </a>
+                        <a href="#" className="mx-5">
+                            <FaFacebookF className="text-white text-2xl hover:text-yellow-400 duration-300" />
+                        </a>
+                        <a href="#">
+                            <FaXTwitter className="text-white text-2xl hover:text-yellow-400 duration-300" />
+                        </a>
+                    </div>
+                </div>
+
+                <div className="flex-col justify-center mx-auto">
+                    <div className="text-2xl font-semibold text-white">
+                        Useful Links
+                    </div>
+                    <ul className="ml-3 text-gray-300 mt-4 grid grid-cols-1 gap-2">
+                        <li className="hover:text-yellow-400 duration-300">
+                            <a href="#">Terms & Conditions</a>
+                        </li>
+                        <li className="hover:text-yellow-400 duration-300">
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li className="hover:text-yellow-400 duration-300">
+                            <a href="#">Refund Policy</a>
+                        </li>
+                        <li className="hover:text-yellow-400 duration-300">
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            {/* Copyright */}
+            <div className="mt-10">
+                <div className="text-gray-500 border-t border-gray-700 lg:w-[65%] pt-3">
+                    © Copyright 2024 Kanakdhara Jewellers. All Rights Reserved.
+                </div>
+            </div>
         </div>
     )
 }
